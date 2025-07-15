b9e54f8ba640ee43a5c21b0be60ac8d5cb183135
Nyantakyi-Francis <nyantakyifrancis54@gmail.com@byupathway.edu>
2025-07-15 13:58:59 +0000

    w3

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 /// <summary>
 /// Defines a maze using a dictionary. The dictionary is provided by the
 /// user when the Maze object is created. The dictionary will contain the
@@ -9,9 +12,9 @@
 /// 'left', 'right', 'up', and 'down' are boolean are represent valid directions
 ///
 /// If a direction is false, then we can assume there is a wall in that direction.
-/// If a direction is true, then we can proceed.  
+/// If a direction is true, then we can proceed.
 ///
-/// If there is a wall, then throw an InvalidOperationException with the message "Can't go that way!".  If there is no wall,
+/// If there is a wall, then throw an InvalidOperationException with the message "Can't go that way!". If there is no wall,
 /// then the 'currX' and 'currY' values should be changed.
 /// </summary>
 public class Maze
@@ -25,41 +28,98 @@ public class Maze
         _mazeMap = mazeMap;
     }
 
-    // TODO Problem 4 - ADD YOUR CODE HERE
+    // Problem 4 - ADD YOUR CODE HERE
     /// <summary>
-    /// Check to see if you can move left.  If you can, then move.  If you
+    /// Check to see if you can move left. If you can, then move. If you
     /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
     /// </summary>
     public void MoveLeft()
     {
-        // FILL IN CODE
+        if (_mazeMap.TryGetValue((_currX, _currY), out bool[] movements))
+        {
+            if (movements[0]) // Index 0 is 'left'
+            {
+                _currX--;
+            }
+            else
+            {
+                throw new InvalidOperationException("Can't go that way!");
+            }
+        }
+        else
+        {
+            // This case should ideally not happen if mazeMap is well-formed
+            throw new InvalidOperationException("Invalid maze location.");
+        }
     }
 
     /// <summary>
-    /// Check to see if you can move right.  If you can, then move.  If you
+    /// Check to see if you can move right. If you can, then move. If you
     /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
     /// </summary>
     public void MoveRight()
     {
-        // FILL IN CODE
+        if (_mazeMap.TryGetValue((_currX, _currY), out bool[] movements))
+        {
+            if (movements[1]) // Index 1 is 'right'
+            {
+                _currX++;
+            }
+            else
+            {
+                throw new InvalidOperationException("Can't go that way!");
+            }
+        }
+        else
+        {
+            throw new InvalidOperationException("Invalid maze location.");
+        }
     }
 
     /// <summary>
-    /// Check to see if you can move up.  If you can, then move.  If you
+    /// Check to see if you can move up. If you can, then move. If you
     /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
     /// </summary>
     public void MoveUp()
     {
-        // FILL IN CODE
+        if (_mazeMap.TryGetValue((_currX, _currY), out bool[] movements))
+        {
+            if (movements[2]) // Index 2 is 'up'
+            {
+                _currY--;
+            }
+            else
+            {
+                throw new InvalidOperationException("Can't go that way!");
+            }
+        }
+        else
+        {
+            throw new InvalidOperationException("Invalid maze location.");
+        }
     }
 
     /// <summary>
-    /// Check to see if you can move down.  If you can, then move.  If you
+    /// Check to see if you can move down. If you can, then move. If you
     /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
     /// </summary>
     public void MoveDown()
     {
-        // FILL IN CODE
+        if (_mazeMap.TryGetValue((_currX, _currY), out bool[] movements))
+        {
+            if (movements[3]) // Index 3 is 'down'
+            {
+                _currY++;
+            }
+            else
+            {
+                throw new InvalidOperationException("Can't go that way!");
+            }
+        }
+        else
+        {
+            throw new InvalidOperationException("Invalid maze location.");
+        }
     }
 
     public string GetStatus()
--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -1,11 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
 
 public static class SetsAndMaps
 {
     /// <summary>
-    /// The words parameter contains a list of two character 
-    /// words (lower case, no duplicates). Using sets, find an O(n) 
-    /// solution for returning all symmetric pairs of words.  
+    /// The words parameter contains a list of two character
+    /// words (lower case, no duplicates). Using sets, find an O(n)
+    /// solution for returning all symmetric pairs of words.
     ///
     /// For example, if words was: [am, at, ma, if, fi], we would return :
     ///
@@ -21,17 +26,34 @@ public static class SetsAndMaps
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     public static string[] FindPairs(string[] words)
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        var wordSet = new HashSet<string>(words);
+        var result = new List<string>();
+
+        foreach (var word in words)
+        {
+            // Optimized reversal for 2-character words to improve efficiency
+            // Instead of creating a new string and array, we directly swap characters.
+            char c1 = word[0];
+            char c2 = word[1];
+            string reversedWord = $"{c2}{c1}"; // String interpolation is efficient for small strings
+
+            // Check if the word is not a palindrome (e.g., "aa") and its reverse exists in the set
+            // Also, ensure we only add one version of the pair (e.g., "am & ma" but not "ma & am")
+            if (word != reversedWord && wordSet.Contains(reversedWord) && string.CompareOrdinal(word, reversedWord) < 0)
+            {
+                result.Add($"{word} & {reversedWord}");
+            }
+        }
+        return result.ToArray();
     }
 
     /// <summary>
     /// Read a census file and summarize the degrees (education)
-    /// earned by those contained in the file.  The summary
+    /// earned by those contained in the file. The summary
     /// should be stored in a dictionary where the key is the
-    /// degree earned and the value is the number of people that 
-    /// have earned that degree.  The degree information is in
-    /// the 4th column of the file.  There is no header row in the
+    /// degree earned and the value is the number of people that
+    /// have earned that degree. The degree information is in
+    /// the 4th column of the file. There is no header row in the
     /// file.
     /// </summary>
     /// <param name="filename">The name of the file to read</param>
@@ -42,47 +64,97 @@ public static class SetsAndMaps
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            // Problem 2 - ADD YOUR CODE HERE
+            if (fields.Length > 3) // Column 4 is at index 3
+            {
+                var degree = fields[3].Trim();
+                if (degrees.ContainsKey(degree))
+                {
+                    degrees[degree]++;
+                }
+                else
+                {
+                    degrees[degree] = 1;
+                }
+            }
         }
 
         return degrees;
     }
 
     /// <summary>
-    /// Determine if 'word1' and 'word2' are anagrams.  An anagram
-    /// is when the same letters in a word are re-organized into a 
-    /// new word.  A dictionary is used to solve the problem.
-    /// 
+    /// Determine if 'word1' and 'word2' are anagrams. An anagram
+    /// is when the same letters in a word are re-organized into a
+    /// new word. A dictionary is used to solve the problem.
+    ///
     /// Examples:
     /// is_anagram("CAT","ACT") would return true
     /// is_anagram("DOG","GOOD") would return false because GOOD has 2 O's
-    /// 
+    ///
     /// Important Note: When determining if two words are anagrams, you
-    /// should ignore any spaces.  You should also ignore cases.  For 
+    /// should ignore any spaces. You should also ignore cases. For
     /// example, 'Ab' and 'Ba' should be considered anagrams
-    /// 
-    /// Reminder: You can access a letter by index in a string by 
+    ///
+    /// Reminder: You can access a letter by index in a string by
     /// using the [] notation.
     /// </summary>
     public static bool IsAnagram(string word1, string word2)
     {
-        // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+        // Problem 3 - ADD YOUR CODE HERE
+        var charCount1 = new Dictionary<char, int>();
+        var charCount2 = new Dictionary<char, int>();
+
+        Action<string, Dictionary<char, int>> populateCharCounts = (word, charCounts) =>
+        {
+            foreach (char c in word)
+            {
+                if (char.IsWhiteSpace(c))
+                    continue;
+
+                char lowerC = char.ToLower(c);
+                if (charCounts.ContainsKey(lowerC))
+                {
+                    charCounts[lowerC]++;
+                }
+                else
+                {
+                    charCounts[lowerC] = 1;
+                }
+            }
+        };
+
+        populateCharCounts(word1, charCount1);
+        populateCharCounts(word2, charCount2);
+
+        if (charCount1.Count != charCount2.Count)
+        {
+            return false;
+        }
+
+        foreach (var pair in charCount1)
+        {
+            if (!charCount2.ContainsKey(pair.Key) || charCount2[pair.Key] != pair.Value)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /// <summary>
-    /// This function will read JSON (Javascript Object Notation) data from the 
+    /// This function will read JSON (Javascript Object Notation) data from the
     /// United States Geological Service (USGS) consisting of earthquake data.
     /// The data will include all earthquakes in the current day.
-    /// 
+    ///
     /// JSON data is organized into a dictionary. After reading the data using
     /// the built-in HTTP client library, this function will return a list of all
     /// earthquake locations ('place' attribute) and magnitudes ('mag' attribute).
-    /// Additional information about the format of the JSON data can be found 
-    /// at this website:  
-    /// 
+    /// Additional information about the format of the JSON data can be found
+    /// at this website:
+    ///
     /// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-    /// 
+    ///
     /// </summary>
     public static string[] EarthquakeDailySummary()
     {
@@ -96,11 +168,22 @@ public static class SetsAndMaps
 
         var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
 
-        // TODO Problem 5:
-        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
+        // Problem 5:
+        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties
         // on those classes so that the call to Deserialize above works properly.
         // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
         // 3. Return an array of these string descriptions.
-        return [];
+        var earthquakeSummaries = new List<string>();
+        if (featureCollection?.Features != null)
+        {
+            foreach (var feature in featureCollection.Features)
+            {
+                if (feature.Properties != null)
+                {
+                    earthquakeSummaries.Add($"{feature.Properties.Place} - Mag {feature.Properties.Mag}");
+                }
+            }
+        }
+        return earthquakeSummaries.ToArray();
     }
 }
